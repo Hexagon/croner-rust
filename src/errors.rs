eb811c127c1412a8d081053c72879749268467a2
Hexagon <hexagon@56k.guru>
2024-01-09 20:52:01 +0000

    Documentation of errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,13 +1,46 @@
-// Define a generic error enum that can be used throughout the croner crate
+/// Represents errors that can occur while parsing and evaluating cron patterns.
+///
+/// `CronError` is used throughout the `croner` crate to indicate various types of failures
+/// and is exported for consuming programs to use.
 #[derive(Debug)]
 pub enum CronError {
+    /// The pattern string provided was empty.
+    ///
+    /// This error occurs if a cron pattern is set to an empty string, which is not a valid cron expression.
     EmptyPattern,
+
+    /// Encountered an invalid date while parsing or evaluating a cron pattern.
+    ///
+    /// This might happen if a cron pattern results in a date that doesn't exist (e.g., February 30th).
     InvalidDate,
+
+    /// Encountered an invalid time while parsing or evaluating a cron pattern.
+    ///
+    /// This can occur if a time component in the cron pattern is outside its valid range.
     InvalidTime,
+
+    /// The search for the next valid time exceeded a reasonable limit.
+    ///
+    /// This is typically encountered with complex patterns that don't match any real-world times.
     TimeSearchLimitExceeded,
+
+    /// The cron pattern provided is invalid.
+    ///
+    /// This error includes a message detailing the nature of the invalid pattern,
+    /// such as "Pattern must consist of six fields, seconds can not be omitted."
     InvalidPattern(String),
+
+    /// The pattern contains characters that are not allowed.
+    ///
+    /// This error includes a message indicating the illegal characters encountered in the pattern,
+    /// such as "CronPattern contains illegal characters."
     IllegalCharacters(String),
-    ComponentError(String), // Used for various errors specifically from `CronComponent`
+
+    /// A component of the pattern is invalid.
+    ///
+    /// This variant is used for various errors that specifically arise from individual components of a cron pattern,
+    /// such as "Position x is out of bounds for the current range (y-z).".
+    ComponentError(String),
 }
 impl std::fmt::Display for CronError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
